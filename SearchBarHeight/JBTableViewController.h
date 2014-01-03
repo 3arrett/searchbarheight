bdcdf7250979806ccb4c22d219f5acd59a07d894
Joe Barrett <joe@3arrett.com>
2014-01-03 00:33:39 -0600

    Added some of the suggestions

---
+++ SearchBarHeight/JBTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JBTableViewController.h
+//  SearchBarHeight
+//
+//  Created by Joe Barrett on 1/3/14.
+//  Copyright (c) 2014 Joseph + Allan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JBTableViewController : UITableViewController <UISearchBarDelegate, UISearchDisplayDelegate>
+
+@end
---
+++ SearchBarHeight/JBTableViewController.m
@@ -0,0 +1,75 @@
+//
+//  JBTableViewController.m
+//  SearchBarHeight
+//
+//  Created by Joe Barrett on 1/3/14.
+//  Copyright (c) 2014 Joseph + Allan. All rights reserved.
+//
+
+#import "JBTableViewController.h"
+
+@interface JBTableViewController ()
+
+@property (strong, nonatomic) NSArray *tableItems;
+@property (strong, nonatomic) UITableView *tableView;
+
+@end
+
+@implementation JBTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    self.tableItems = @[ @"Box", @"Table", @"Cardboard" ];
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.tableItems.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text = self.tableItems[indexPath.row];
+    
+    return cell;
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar setFrame:CGRectMake(0, 0, 320, 44)];
+}
+
+@end
